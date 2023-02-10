7c47c1646b7f0254648ed58df43f22080ba32a8b
Joao <joao.robertofg@gmail.com>
2023-02-10 04:25:38 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    autoplay: {
+        delay: 2000,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+  });
+
+	
